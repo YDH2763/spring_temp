2d9a1e23ac61fa21d71c2989b1d83e7f76e3d456
YDH2763 <dbehdgns12345@gmail.com>
2025-03-24 12:49:15 +0900

    회원 가입 구현 (비번 암호화 적용 x)

--- src/main/java/kr/kh/temp/controller/HomeController.java
+++ src/main/java/kr/kh/temp/controller/HomeController.java
@@ -9,9 +9,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.kh.temp.model.vo.MemberVO;
 import kr.kh.temp.service.MemberService;
 
 
@@ -26,6 +29,22 @@ public class HomeController {
 	public String home() {
 		
 		return "home";
+	}
+	
+	@GetMapping("/signup")
+	public String signup() {
+		
+		return "/member/signup";
+	}
+	
+	@PostMapping("/signup")
+	public String signupPost(MemberVO member) {
+		if(memberService.signup(member)) {
+			
+		}else {
+			
+		}
+		return "/member/signup";
 	}
 	
 }
