de9fe037ea20167e2d7c4d2ab8cfa05785af2bfa
YDH2763 <dbehdgns12345@gmail.com>
2025-03-25 09:40:29 +0900

    게시판 등록 구현 완료

--- src/main/java/kr/kh/temp/dao/PostDAO.java
+++ src/main/java/kr/kh/temp/dao/PostDAO.java
@@ -1,5 +1,9 @@
 package kr.kh.temp.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface PostDAO {
+
+	boolean insertBoard(@Param("name")String name);
 
 }
--- src/main/java/kr/kh/temp/service/PostService.java
+++ src/main/java/kr/kh/temp/service/PostService.java
@@ -3,4 +3,6 @@ package kr.kh.temp.service;
 
 public interface PostService {
 
+	boolean insertBoard(String name);
+
 }
